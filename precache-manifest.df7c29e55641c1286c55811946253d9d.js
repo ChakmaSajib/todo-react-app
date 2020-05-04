48f81e2a7e94ec05c57391f4249d6470bd3ac77f
Sajib Chakma <chakma.sajib95@yahoo.com>
2020-05-04 18:20:41 +0800

    Updates

---
+++ precache-manifest.df7c29e55641c1286c55811946253d9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31eb13335be4596d6dd15dadd7cc83f4",
+    "url": "/todo-react-app/index.html"
+  },
+  {
+    "revision": "4e28c876f1eb5162be73",
+    "url": "/todo-react-app/static/css/main.36e11047.chunk.css"
+  },
+  {
+    "revision": "911a75197ef85800aa7d",
+    "url": "/todo-react-app/static/js/2.939aa468.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react-app/static/js/2.939aa468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e28c876f1eb5162be73",
+    "url": "/todo-react-app/static/js/main.303ec385.chunk.js"
+  },
+  {
+    "revision": "cfab79ceb477949157cd",
+    "url": "/todo-react-app/static/js/runtime-main.68c75ef0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
